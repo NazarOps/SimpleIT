b58d95bb6abc1238b8966aa4e7680acc632e1410
NazarOps <lilnz3@hotmail.com>
2017-02-08 13:40:48 +0100

    fix

--- mySQL_connect.php
+++ mySQL_connect.php
@@ -6,10 +6,11 @@
 	DEFINE('DB_PASSWORD','');
 	DEFINE('DB_DATABASE','SimpleITUF');
 
+	$dbc = mysqli_connect(DB_HOST,DB_USER,DB_PASSWORD);
+	
+	mysqli_query($dbc,"CREATE DATABASE IF NOT EXISTS simpleituf;");
+	
 	$dbc = mysqli_connect(DB_HOST,DB_USER,DB_PASSWORD,DB_DATABASE);
-	
-	mysqli_query($dbc,"CREATE DATABASE simpleit IF NOT EXISTS;");
-	
-	
+
 	
 ?>
